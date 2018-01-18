8de27a4bf547b836397a9ca037891842fa1f4e26
ellie.rabbit <ellie.rabbit@daumkakao.com>
2018-01-18 22:06:51 +0900

    ADD: 로그백 설정 추가

--- src/main/java/com/adinstar/pangyo/controller/api/TestApiContoller.java
+++ src/main/java/com/adinstar/pangyo/controller/api/TestApiContoller.java
@@ -2,16 +2,21 @@ package com.adinstar.pangyo.controller.api;
 
 import com.adinstar.pangyo.model.Test;
 import com.adinstar.pangyo.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/api/test")
 public class TestApiContoller {
     @Autowired
     private TestService testService;
+
+    static final Logger log = LoggerFactory.getLogger(TestApiContoller.class);
 
     @RequestMapping("/string")
     public String string() {
@@ -30,6 +35,7 @@ public class TestApiContoller {
 
     @RequestMapping("/model")
     public Test model(@RequestParam Integer val1) {
+        log.debug("GET model (param:{})", val1);
         return testService.get(val1);
     }
 }
