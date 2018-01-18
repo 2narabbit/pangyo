d5d81755aa6acac9260aed5d6eb61ad999576340
ellie.rabbit <ellie.rabbit@daumkakao.com>
2018-01-18 21:48:04 +0900

    UPD: TEST DB row를 반환하는 API 추가

--- src/main/java/com/adinstar/pangyo/controller/api/TestApiContoller.java
+++ src/main/java/com/adinstar/pangyo/controller/api/TestApiContoller.java
@@ -1,12 +1,18 @@
 package com.adinstar.pangyo.controller.api;
 
 import com.adinstar.pangyo.model.Test;
+import com.adinstar.pangyo.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/test")
 public class TestApiContoller {
+    @Autowired
+    private TestService testService;
+
     @RequestMapping("/string")
     public String string() {
         return "Greetings from Spring Boot!";
@@ -20,5 +26,10 @@ public class TestApiContoller {
         test.setVal3(true);
 
         return test;
+    }
+
+    @RequestMapping("/model")
+    public Test model(@RequestParam Integer val1) {
+        return testService.get(val1);
     }
 }
