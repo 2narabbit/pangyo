0429d20bd8522fc847dd9ad7a954ffa9d62befde
ellie.rabbit <ellie.rabbit@daumkakao.com>
2018-01-18 18:17:58 +0900

    UPD: 객체를 반환하는 API 추가

--- src/main/java/com/adinstar/pangyo/controller/api/TestApiContoller.java
+++ src/main/java/com/adinstar/pangyo/controller/api/TestApiContoller.java
@@ -1,13 +1,24 @@
 package com.adinstar.pangyo.controller.api;
 
+import com.adinstar.pangyo.model.Test;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/test")
 public class TestApiContoller {
-    @RequestMapping("/index")
-    public String index() {
+    @RequestMapping("/string")
+    public String string() {
         return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping("/object")
+    public Test object() {
+        Test test = new Test();
+        test.setVal1(1);
+        test.setVal2("ellie");
+        test.setVal3(true);
+
+        return test;
     }
 }
