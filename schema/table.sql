ab60474add50794c629f8d86d9b1b612e3114ba5
2narabbit <eveznar@gmail.com>
2018-02-01 13:14:01 +0900

    UPD: 집행룰 status enum value 변경

--- schema/table.sql
+++ schema/table.sql
@@ -77,7 +77,7 @@ create table EXECUTION_RULE
 	type enum('CAMPAIGN', 'CANDIDATE', 'AD') not null,
 	start_dttm datetime not null,
 	end_dttm datetime not null,
-	status enum('BEFORE', 'RUNNING', 'STOP', 'END') not null comment 'ENUM(''BEFORE'', ''RUNNING'', ''STOP'', ''END'')
+	status enum('READY', 'RUNNING', 'DONE', 'END') not null comment 'ENUM(''BEFORE'', ''RUNNING'', ''STOP'', ''END'')
 	—> ENUM(‘READY’, ''RUNNING'', ‘DONE’, ''END'')',
 	reg_dttm datetime default CURRENT_TIMESTAMP not null,
 	up_dttm datetime default CURRENT_TIMESTAMP not null,
