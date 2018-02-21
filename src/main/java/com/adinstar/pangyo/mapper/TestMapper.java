9dac0053dac44d68c0fd3fb060355299bf2fdf9f
2narabbit <eveznar@gmail.com>
2018-02-21 14:23:52 +0900

    DEL: test model 삭제에 따른 test관련 class 제거

--- src/main/java/com/adinstar/pangyo/mapper/TestMapper.java
+++
@@ -1,9 +0,0 @@
-package com.adinstar.pangyo.mapper;
-
-import com.adinstar.pangyo.model.Test;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TestMapper {
-    Test select(Integer val1);
-}
