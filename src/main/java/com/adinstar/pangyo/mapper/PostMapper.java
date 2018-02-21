0621b34e9440824f9ac1b8d17f0e17cb7cbd0abe
2narabbit <eveznar@gmail.com>
2018-02-21 16:45:25 +0900

    UPD: post delete query 추가

--- src/main/java/com/adinstar/pangyo/mapper/PostMapper.java
+++ src/main/java/com/adinstar/pangyo/mapper/PostMapper.java
@@ -9,10 +9,8 @@ import java.util.List;
 @Mapper
 public interface PostMapper {
     List<Post> selectList(@Param("startOffset") int startOffset, @Param("endOffset") int endOffset);
-
     Post selectById(int id);
-
     int insert(Post post);
-
-    void update(Post post);
+    int update(Post post);
+    int delete(Integer postId);
 }
