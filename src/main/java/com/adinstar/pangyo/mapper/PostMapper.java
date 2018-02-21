6d02c7b8f7befb04877ea4ebb968174aa3da7246
2narabbit <eveznar@gmail.com>
2018-02-21 17:03:57 +0900

    ADD: 스타ID로 list 가져오는 쿼리 추가

--- src/main/java/com/adinstar/pangyo/mapper/PostMapper.java
+++ src/main/java/com/adinstar/pangyo/mapper/PostMapper.java
@@ -9,6 +9,7 @@ import java.util.List;
 @Mapper
 public interface PostMapper {
     List<Post> selectList(@Param("startOffset") int startOffset, @Param("endOffset") int endOffset);
+    List<Post> selectListByStarId(@Param("starId") int starId, @Param("startOffset") int startOffset, @Param("endOffset") int endOffset);
     Post selectById(int id);
     int insert(Post post);
     int update(Post post);
