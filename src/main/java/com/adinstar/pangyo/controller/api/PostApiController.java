e24dfa734f26623c6f26c3a3999675a87c0f032b
2narabbit <eveznar@gmail.com>
2018-02-21 17:05:59 +0900

    ADD: 포스트 상세, 포스트 리스트 API 추가

--- src/main/java/com/adinstar/pangyo/controller/api/PostApiController.java
+++ src/main/java/com/adinstar/pangyo/controller/api/PostApiController.java
@@ -1,16 +1,17 @@
 package com.adinstar.pangyo.controller.api;
 
 
+import com.adinstar.pangyo.helper.PageHelper;
 import com.adinstar.pangyo.model.Post;
 import com.adinstar.pangyo.service.PostService;
 import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/post")
@@ -20,17 +21,36 @@ public class PostApiController {
     @Autowired
     private PostService postService;
 
-    @ApiOperation("getModel")
+    @ApiOperation("getPostListByStarId")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "val1", value = "value one", paramType = "query", dataType = "Integer")
+            @ApiImplicitParam(name="starId", value="star id", paramType="query", dataType="Integer"),
+            @ApiImplicitParam(name="page", value="page number", paramType="query", dataType="Integer"),
+            @ApiImplicitParam(name="size", value="page size", paramType="query", dataType="Integer")
     })
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Success ^-^", response = Post.class),
-            @ApiResponse(code = 404, message = "Not Found T.T")
+            @ApiResponse(code = 200, message = "OK", response = List.class)
     })
-    @RequestMapping(value = "/model", method = RequestMethod.GET)
-    public Post model(@RequestParam Integer val1) {
-        logger.debug("GET model (param:{})", val1);
-        return postService.findById(val1);
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public List<Post> getPostListByStarId(@RequestParam(value = "starId", required = false) Integer starId,
+                                          @RequestParam(value = "page", defaultValue = "1") Integer page,
+                                          @RequestParam(value = "size", defaultValue = "20") Integer size) {
+        if (starId == null) {
+            return postService.findAll(new PageHelper(page, size));
+        } else {
+            return postService.findAllByStarId(starId, new PageHelper(page, size));
+        }
+    }
+
+    @ApiOperation("getPost")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="postId", value="post id", paramType="path", required=true, dataType="Integer")
+    })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK", response = Post.class),
+            @ApiResponse(code = 404, message = "Not Found")
+    })
+    @RequestMapping(value = "/{postId}", method = RequestMethod.GET)
+    public Post getPost(@PathVariable("postId") Integer postId) {
+        return postService.findById(postId);
     }
 }
--- src/main/java/com/adinstar/pangyo/service/PostService.java
+++ src/main/java/com/adinstar/pangyo/service/PostService.java
@@ -18,6 +18,10 @@ public class PostService {
         return postMapper.selectList(pageHelper.getStartOffset(), pageHelper.getEndOffset());
     }
 
+    public List<Post> findAllByStarId(int starId, PageHelper pageHelper) {
+        return postMapper.selectListByStarId(starId, pageHelper.getStartOffset(), pageHelper.getEndOffset());
+    }
+
     public Post findById(int id) {
         return postMapper.selectById(id);
     }
@@ -28,6 +32,12 @@ public class PostService {
     }
 
     public void modify(Post post) {
+        // TODO : user 권한 체크
         postMapper.update(post);
+    }
+
+    public void delete(Integer postId) {
+        // TODO : user 권한 체크
+        postMapper.delete(postId);
     }
 }
