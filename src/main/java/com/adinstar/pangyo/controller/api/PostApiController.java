35b8f775668d8854f6b48086f70640503df9cdc4
2narabbit <eveznar@gmail.com>
2018-02-23 14:06:28 +0900

    DEL: 게시글 작성/수정 API 제거
    - 게시글작성/수정을 submit 방식으로 가기로 협의하여 불필요한 코드 제거

--- src/main/java/com/adinstar/pangyo/controller/api/PostApiController.java
+++ src/main/java/com/adinstar/pangyo/controller/api/PostApiController.java
@@ -3,7 +3,6 @@ package com.adinstar.pangyo.controller.api;
 
 import com.adinstar.pangyo.model.Post;
 import com.adinstar.pangyo.service.PostService;
-import com.google.common.collect.ImmutableMap;
 import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 
 @RestController
@@ -51,41 +49,5 @@ public class PostApiController {
     @RequestMapping(value = "/{postId}", method = RequestMethod.GET)
     public Post get(@PathVariable("postId") long postId) {
         return postService.findById(postId);
-    }
-
-    //
-    // test : curl -X POST -H "Content-Type: application/json" -d '{"starId":2, "user":{"id":2}, "body":"api test"}' http://localhost:8080/api/post
-    //
-    @ApiOperation("addPost")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name="post", value="post object", paramType="body", required=true, dataType="Post")
-    })
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "OK", response = Map.class)
-    })
-    @RequestMapping(method = RequestMethod.POST)
-    public Map<Object, Object> add(@RequestBody Post post) {
-        long postId = postService.save(post);
-
-        // FIXME: API 응답 객체 있으면 좋을 것 같은데.. 어떻게 만들어야 예쁠지?
-        return ImmutableMap.builder().put("id", postId).build();
-    }
-
-    //
-    // test : curl -X PUT -H "Content-Type: application/json" -d '{"id":15, "body":"api modify test", "img":"https://t1.daumcdn.net/news/201802/21/seouleconomy/20180221112112176gmfz.jpg"}' http://localhost:8080/api/post
-    //
-    @ApiOperation("modifyPost")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name="post", value="post object", paramType="body", required=true, dataType="Post")
-    })
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "OK", response = Map.class)
-    })
-    @RequestMapping(method = RequestMethod.PUT)
-    public Map<Object, Object> modify(@RequestBody Post post) {
-        int rv = postService.modify(post);
-
-        // FIXME: API 응답 객체 있으면 좋을 것 같은데.. 어떻게 만들어야 예쁠지?
-        return ImmutableMap.builder().put("rv", rv).build();
     }
 }
