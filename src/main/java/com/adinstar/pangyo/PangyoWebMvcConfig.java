679ee6613966980306a24bc600da8d6d33339159
ellie.rabbit <ellie.rabbit@daumkakao.com>
2018-03-25 20:37:46 +0900

    리소스 파일 intercept 제외

--- src/main/java/com/adinstar/pangyo/PangyoWebMvcConfig.java
+++ src/main/java/com/adinstar/pangyo/PangyoWebMvcConfig.java
@@ -3,10 +3,7 @@ package com.adinstar.pangyo;
 import com.adinstar.pangyo.controller.interceptor.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @EnableWebMvc
@@ -19,11 +16,17 @@ public class PangyoWebMvcConfig extends WebMvcConfigurerAdapter {
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
     }
-
-
+    
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/**");
+                .addPathPatterns("/api/**")
+                .addPathPatterns("/fanClub/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js/");
     }
 }
